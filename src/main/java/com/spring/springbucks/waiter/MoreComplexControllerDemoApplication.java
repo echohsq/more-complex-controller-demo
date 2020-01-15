de3d2939e8be0818c4017bb29302325397cad16a
郝盛强 <hsqzs1996@gmail.com>
2020-01-15 23:46:03 +0800

    上传pom.xml文件

---
+++ src/main/java/com/spring/springbucks/waiter/MoreComplexControllerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springbucks.waiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreComplexControllerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoreComplexControllerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/springbucks/waiter/MoreComplexControllerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbucks.waiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreComplexControllerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
